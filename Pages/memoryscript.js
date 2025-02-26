6e7c92ce21f878f0e8c038a26a9b397a48c9b0c3
SizzleTheSoda <satori.beasley@outlook.com>
2025-02-26 17:19:34 -0600

    adding fetch to card.json

--- Pages/memoryscript.js
+++ Pages/memoryscript.js
@@ -6,43 +6,4 @@ let score = 0;
 
 document.querySelector(".score").textContext = score;
 
-fetch("")
-
-[
-    {
-        "image": "../assets/chili.png",
-        "name": "chili"
-    },
-    {
-        "image": "../assets/grapes.png",
-        "name": "grapes"
-    },
-    {
-        "image": "../assets/lemon.png",
-        "name": "lemon"
-    },
-    {
-        "image": "../assets/orange.png",
-        "name": "orange"
-    },
-    {
-        "image": "../assets/pineapple.png",
-        "name": "pineapple"
-    },
-    {
-        "image": "../assets/strawberry.png",
-        "name": "strawberry"
-    },
-    {
-        "image": "../assets/tomato.png",
-        "name": "tomato"
-    },
-    {
-        "image": "../assets/watermelon.png",
-        "name": "watermelon"
-    },
-    {
-        "image": "../assets/cherries.png",
-        "name": "cherries"
-    }
-]
+fetch("cards.json")
