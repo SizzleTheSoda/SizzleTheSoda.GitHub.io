4d3feb5c181b1b1144e0f5e0d3bb13ed54693382
SizzleTheSoda <satori.beasley@outlook.com>
2025-02-26 17:13:43 -0600

    Adding images thrrough json for memorygame

---
+++ Pages/memoryscript.js
@@ -0,0 +1,48 @@
+const gridContainer =cdocument.cdocument.querySelector(".grid-container");
+let cards= [];
+let firstcard, secondcard;
+lockboard = false;
+let score = 0; 
+
+document.querySelector(".score").textContext = score;
+
+fetch("")
+
+[
+    {
+        "image": "../assets/chili.png",
+        "name": "chili"
+    },
+    {
+        "image": "../assets/grapes.png",
+        "name": "grapes"
+    },
+    {
+        "image": "../assets/lemon.png",
+        "name": "lemon"
+    },
+    {
+        "image": "../assets/orange.png",
+        "name": "orange"
+    },
+    {
+        "image": "../assets/pineapple.png",
+        "name": "pineapple"
+    },
+    {
+        "image": "../assets/strawberry.png",
+        "name": "strawberry"
+    },
+    {
+        "image": "../assets/tomato.png",
+        "name": "tomato"
+    },
+    {
+        "image": "../assets/watermelon.png",
+        "name": "watermelon"
+    },
+    {
+        "image": "../assets/cherries.png",
+        "name": "cherries"
+    }
+]
